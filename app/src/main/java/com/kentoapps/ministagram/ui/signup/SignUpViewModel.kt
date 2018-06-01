445811cea9c455d58ddbc6d2e6163620e390f8d1
Kento Uchida <kento.apps@gmail.com>
2018-06-01 16:26:23 -0700

    Implement sing up feature for vm, temporary

--- app/src/main/java/com/kentoapps/ministagram/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/kentoapps/ministagram/ui/signup/SignUpViewModel.kt
@@ -1,7 +1,28 @@
 package com.kentoapps.ministagram.ui.signup
 
-import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import com.kentoapps.ministagram.data.source.user.UserRepository
+import io.reactivex.rxkotlin.subscribeBy
 
 class SignUpViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val userName = ObservableField<String>()
+    val email = ObservableField<String>()
+    val password = ObservableField<String>()
+    val confirm = ObservableField<String>()
+
+    fun onSignUpClick() {
+        // TODO Should be injected by Dagger
+        val repository = UserRepository()
+        // TODO Validate values
+        repository.signUp(userName.get()!!, email.get()!!, password.get()!!)
+                .subscribeBy(
+                        onComplete = {
+                            println("=== complete")
+                        },
+                        onError = {
+                            println("=== error: ${it.message}")
+                        }
+                )
+    }
 }
