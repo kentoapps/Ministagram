27e3d1dfd5cb5bd9e53fbba5e068a0a45e43605f
Kento Uchida <kento.apps@gmail.com>
2018-06-01 02:22:39 -0700

    Implement extension for createUserWithEmail on Firebase

---
+++ app/src/main/java/com/kentoapps/ministagram/util/extension/FirebaseExtension.kt
@@ -0,0 +1,12 @@
+package com.kentoapps.ministagram.util.extension
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+fun FirebaseAuth.createUser(email: String,
+                            password: String,
+                            complete: (Task<AuthResult>) -> Unit) {
+    createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(complete)
+}
