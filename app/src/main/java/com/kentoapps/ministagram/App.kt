5f53ef462d1f566872226c64692ff4e41bb47538
Kento Uchida <kento.apps@gmail.com>
2018-06-10 00:17:23 -0700

    Modify AppComponent

--- app/src/main/java/com/kentoapps/ministagram/App.kt
+++ app/src/main/java/com/kentoapps/ministagram/App.kt
@@ -2,17 +2,14 @@ package com.kentoapps.ministagram
 
 import com.kentoapps.ministagram.di.applyAutoInjector
 import com.kentoapps.ministagram.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class App : DaggerApplication() {
 
-//    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-//        return DaggerAppComponent.builder().create(this)
-//    }
-
-    override fun applicationInjector() = DaggerAppComponent.builder()
-            .application(this)
-            .build()
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().create(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/kentoapps/ministagram/di/component/AppComponent.kt
+++ app/src/main/java/com/kentoapps/ministagram/di/component/AppComponent.kt
@@ -3,7 +3,6 @@ package com.kentoapps.ministagram.di.component
 import com.kentoapps.ministagram.App
 import com.kentoapps.ministagram.di.module.ActivityModule
 import com.kentoapps.ministagram.di.module.AppModule
-import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
@@ -12,16 +11,6 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [AndroidSupportInjectionModule::class, AppModule::class, ActivityModule::class])
 interface AppComponent : AndroidInjector<App> {
-
     @Component.Builder
-    interface Builder {
-        @BindsInstance
-        fun application(application: App): Builder
-        fun build(): AppComponent
-    }
-
-//    @Component.Builder
-//    abstract class Builder : AndroidInjector.Builder<App>()
-
-    override fun inject(app: App)
+    abstract class Builder : AndroidInjector.Builder<App>()
 }
