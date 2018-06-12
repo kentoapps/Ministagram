d477cdccdd7b101ed3bf90b3c93308254804de16
Kento Uchida <kento.apps@gmail.com>
2018-06-11 22:55:46 -0700

    Modify SignUpViewModel to use interface for repository

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/signup/SignUpViewModel.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/signup/SignUpViewModel.kt
@@ -2,11 +2,11 @@ package com.kentoapps.ministagram.ui.signup
 
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
-import com.kentoapps.ministagram.data.source.user.UserRepository
+import com.kentoapps.ministagram.data.source.user.UserDataSource
 import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
 
-class SignUpViewModel @Inject constructor(private val repository: UserRepository) : ViewModel() {
+class SignUpViewModel @Inject constructor(private val repository: UserDataSource) : ViewModel() {
     val userName = ObservableField<String>()
     val email = ObservableField<String>()
     val password = ObservableField<String>()
