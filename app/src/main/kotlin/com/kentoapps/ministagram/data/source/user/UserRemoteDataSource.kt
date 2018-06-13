7d0f547edb7a55f15ce08a2c977d0d40f5f32935
Kento Uchida <kento.apps@gmail.com>
2018-06-12 18:49:29 -0700

    Implement sign in and out in data source

--- app/src/main/kotlin/com/kentoapps/ministagram/data/source/user/UserRemoteDataSource.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/source/user/UserRemoteDataSource.kt
@@ -2,25 +2,36 @@ package com.kentoapps.ministagram.data.source.user
 
 import com.google.firebase.auth.FirebaseAuth
 import com.kentoapps.ministagram.data.model.User
+import com.kentoapps.ministagram.util.extension.createUser
+import com.kentoapps.ministagram.util.extension.signIn
 import io.reactivex.Completable
 import io.reactivex.Observable
 
 class UserRemoteDataSource : UserDataSource {
     override fun signUp(userName: String, email: String, password: String): Completable {
         return Completable.create { emitter ->
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
+            FirebaseAuth.getInstance()
+                    .createUser(email, password) { task ->
                         if (task.isSuccessful) emitter.onComplete()
-                        else emitter.onError(Throwable(task.exception?.message)) }
+                        else emitter.onError(Throwable(task.exception?.message))
+                    }
         }
     }
 
     override fun signIn(email: String, password: String): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Completable.create { emitter ->
+            FirebaseAuth.getInstance().signIn(email, password) { task ->
+                if (task.isSuccessful) emitter.onComplete()
+                else emitter.onError(Throwable(task.exception?.message))
+            }
+        }
     }
 
     override fun signOut(): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Completable.create { emitter ->
+            FirebaseAuth.getInstance().signOut()
+            emitter.onComplete()
+        }
     }
 
     override fun getUser(): Observable<User> {
