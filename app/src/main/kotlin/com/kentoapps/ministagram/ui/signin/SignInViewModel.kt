f207cb82c88bdc64c5cdca0aaca9542785e25c35
Kento Uchida <kento.apps@gmail.com>
2018-06-12 19:03:52 -0700

    Implement SignInViewModel

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/signin/SignInViewModel.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/signin/SignInViewModel.kt
@@ -1,7 +1,30 @@
 package com.kentoapps.ministagram.ui.signin
 
-import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import com.kentoapps.ministagram.data.source.user.UserDataSource
+import com.kentoapps.ministagram.util.extension.isValidEmail
+import io.reactivex.rxkotlin.subscribeBy
+import javax.inject.Inject
 
-class SignInViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SignInViewModel @Inject constructor(private val repository: UserDataSource) : ViewModel() {
+    val email = ObservableField<String>()
+    val password = ObservableField<String>()
+
+    val errorMessage = ObservableField<String>()
+
+    fun onSignInClick() {
+        if (email.get().isNullOrBlank() || password.get().isNullOrBlank())
+            errorMessage.set("Please fill out all fields")
+        else if (!email.get().isValidEmail())
+            errorMessage.set("Email is invalid")
+        else
+            repository.signIn(email.get()!!, password.get()!!)
+                    .subscribeBy(
+                            onComplete = { println("=== complete") },
+                            onError = {
+                                println("=== error: ${it.message}")
+                                errorMessage.set(it.message)
+                            })
+    }
 }
