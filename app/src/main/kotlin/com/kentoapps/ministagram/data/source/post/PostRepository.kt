f48c7d3b8ef16e55dc7e28350c4a43055629ae0a
Kento Uchida <kento.apps@gmail.com>
2018-06-13 20:08:18 -0700

    Implement PostRepositoryImpl

--- app/src/main/kotlin/com/kentoapps/ministagram/data/source/post/PostRepository.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/source/post/PostRepository.kt
@@ -4,24 +4,10 @@ import com.kentoapps.ministagram.data.model.Post
 import io.reactivex.Completable
 import io.reactivex.Observable
 
-class PostRepository(private val dataSource: PostDataSource) : PostDataSource {
-    override fun getPostList(): Observable<List<Post>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getPost(id: String): Observable<Post> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun savePost(post: Post): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun updatePost(post: Post): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun deletePost(id: String): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+interface PostRepository {
+    fun getPostList(): Observable<List<Post>>
+    fun getPost(id: String): Observable<Post>
+    fun savePost(post: Post): Completable
+    fun updatePost(post: Post): Completable
+    fun deletePost(id: String): Completable
 }
---
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/source/post/PostRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.kentoapps.ministagram.data.source.post
+
+import com.kentoapps.ministagram.data.model.Post
+import io.reactivex.Completable
+import io.reactivex.Observable
+
+class PostRepositoryImpl(private val dataSource: PostDataSource) : PostRepository {
+    override fun getPostList(): Observable<List<Post>> {
+        return dataSource.getPostList()
+    }
+
+    override fun getPost(id: String): Observable<Post> {
+        return dataSource.getPost(id)
+    }
+
+    override fun savePost(post: Post): Completable {
+        return dataSource.savePost(post)
+    }
+
+    override fun updatePost(post: Post): Completable {
+        return dataSource.updatePost(post)
+    }
+
+    override fun deletePost(id: String): Completable {
+        return dataSource.deletePost(id)
+    }
+}
