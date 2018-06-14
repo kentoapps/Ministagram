e5db9f219772837c9d1689ff7b26078c1db6dd27
Kento Uchida <kento.apps@gmail.com>
2018-06-13 18:58:31 -0700

    Rename main_activity to activity_main

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
