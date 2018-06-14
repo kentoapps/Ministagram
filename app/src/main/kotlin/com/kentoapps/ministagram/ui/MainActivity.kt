0246649ef21b603c424a34f1fc07894b6972fed0
Kento Uchida <kento.apps@gmail.com>
2018-06-13 19:04:23 -0700

    Modify MainActivity to use navigation

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.kentoapps.ministagram.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.kentoapps.ministagram.R
-import com.kentoapps.ministagram.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
     }
 
 }
