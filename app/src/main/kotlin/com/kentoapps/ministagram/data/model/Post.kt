8f3896dccad9f3fb65bfe02a4f1b23a6b0fa933a
Kento Uchida <kento.apps@gmail.com>
2018-06-15 02:57:07 -0700

    Modify Post

--- app/src/main/kotlin/com/kentoapps/ministagram/data/model/Post.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/model/Post.kt
@@ -3,10 +3,12 @@ package com.kentoapps.ministagram.data.model
 import java.util.*
 
 data class Post(
+        val id: String,
         val userId: String,
         val userName: String,
         val userImage: String,
-        val Image: String,
+        val image: String,
+        val caption: String,
         val likeUsers: List<User>,
         val numOfComments: Int,
         val date: Date
