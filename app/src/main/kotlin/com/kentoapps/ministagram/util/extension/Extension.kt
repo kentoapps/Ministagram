6a22a22c46b300770b470dcff152f658845bbd3d
Kento Uchida <kento.apps@gmail.com>
2018-06-19 16:23:59 -0700

    Implement withSuffix extension

--- app/src/main/kotlin/com/kentoapps/ministagram/util/extension/Extension.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/util/extension/Extension.kt
@@ -12,3 +12,13 @@ fun String?.isValidEmail(): Boolean =
                                 + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
                                 + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
                 ).matcher(this).matches()
+
+fun Int.withSuffix(suffix: Suffix): String =
+        if (this <= 1)
+            "$this ${suffix.singular}"
+        else
+            "$this ${suffix.plural}"
+
+enum class Suffix(var singular: String, var plural: String) {
+    LIKE("Like", "Likes"), COMMENT("Comment", "Comments")
+}
