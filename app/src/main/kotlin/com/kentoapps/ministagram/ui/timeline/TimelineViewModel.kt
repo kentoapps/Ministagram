db5b5172a9d35d6825b04361934d65b14d660f45
Kento Uchida <kento.apps@gmail.com>
2018-06-19 16:02:44 -0700

    Implement getPostList in VM to subscribe

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/timeline/TimelineViewModel.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/timeline/TimelineViewModel.kt
@@ -2,10 +2,28 @@ package com.kentoapps.ministagram.ui.timeline
 
 import android.arch.lifecycle.ViewModel
 import com.kentoapps.ministagram.data.model.Post
-import com.kentoapps.ministagram.data.source.post.PostRepositoryImpl
+import com.kentoapps.ministagram.data.source.post.PostRepository
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.subjects.BehaviorSubject
 import javax.inject.Inject
 
-class TimelineViewModel @Inject constructor(repository: PostRepositoryImpl): ViewModel() {
+class TimelineViewModel @Inject constructor(private val repository: PostRepository): ViewModel() {
     val posts: BehaviorSubject<List<Post>> = BehaviorSubject.create()
+
+    private val disposables = CompositeDisposable()
+
+    fun getPostList() {
+        repository.getPostList()
+                .subscribeBy (
+                        onNext = { posts.onNext(it) },
+                        onError = { println("=== Error: [getPostList] ${it.message}") }
+                ).addTo(disposables)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
 }
