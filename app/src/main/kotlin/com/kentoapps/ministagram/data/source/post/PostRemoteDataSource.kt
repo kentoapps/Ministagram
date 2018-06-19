250612bc85b7010a62c2a14a521cb5789b91256e
Kento Uchida <kento.apps@gmail.com>
2018-06-19 15:57:33 -0700

    Implement getPostList in DataSource

--- app/src/main/kotlin/com/kentoapps/ministagram/data/source/post/PostRemoteDataSource.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/source/post/PostRemoteDataSource.kt
@@ -1,12 +1,26 @@
 package com.kentoapps.ministagram.data.source.post
 
+import com.google.firebase.firestore.FirebaseFirestore
 import com.kentoapps.ministagram.data.model.Post
+import com.kentoapps.ministagram.util.COLLECTION_POST
 import io.reactivex.Completable
 import io.reactivex.Observable
 
 class PostRemoteDataSource : PostDataSource {
+    private val db = FirebaseFirestore.getInstance()
+
     override fun getPostList(): Observable<List<Post>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Observable.create { emitter ->
+            db.collection(COLLECTION_POST)
+                    .get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            emitter.onNext(task.result.map { it.toObject(Post::class.java) })
+                        } else {
+                            emitter.onError(Throwable("ERROR"))
+                        }
+                    }
+        }
     }
 
     override fun getPost(id: String): Observable<Post> {
