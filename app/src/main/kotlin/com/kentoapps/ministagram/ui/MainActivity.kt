e23ef374ac0153069b33242f6459e737d1f35cee
Kento Uchida <kento.apps@gmail.com>
2018-06-25 13:49:25 -0700

    Modify toolbar icon to menu

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.kentoapps.ministagram.R
@@ -31,9 +34,37 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
         val navController = host.navController
         NavigationUI.setupWithNavController(bottom_nav_view, navController)
+    }
 
-        cameraButton.setOnClickListener {
-            startActivity(Intent(this, CameraActivity::class.java))
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.gallery -> {
+                startActivityForResult(Intent(Intent.ACTION_GET_CONTENT).run { setType("image/*") }, REQUEST_CODE)
+                super.onOptionsItemSelected(item)
+            }
+            R.id.camera_activity -> NavigationUI.onNavDestinationSelected(item,
+                    Navigation.findNavController(this, R.id.nav_host_fragment))
+                    || super.onOptionsItemSelected(item)
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode) {
+            REQUEST_CODE -> {
+                val resultUri = data?.data
+                println("======= Result uri: $resultUri")
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE = 101
     }
 }
