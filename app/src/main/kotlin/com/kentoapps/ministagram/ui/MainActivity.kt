51e20aa63e18a017f61bfbd00b7196ae70cda354
Kento Uchida <kento.apps@gmail.com>
2018-06-27 00:39:09 -0700

    Remove useless comment

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
@@ -38,7 +38,6 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        println("======= onOptions Activity")
         return when (item.itemId) {
             R.id.gallery -> {
                 startActivityForResult(Intent(Intent.ACTION_GET_CONTENT).run { setType("image/*") }, REQUEST_CODE)
