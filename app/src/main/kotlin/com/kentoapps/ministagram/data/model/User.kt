2c65a87b4a2b5e7b3ad21a70404dd639f82b36e9
Kento Uchida <kento.apps@gmail.com>
2018-06-26 17:04:36 -0700

    Modify User data class

--- app/src/main/kotlin/com/kentoapps/ministagram/data/model/User.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/model/User.kt
@@ -1,17 +1,6 @@
 package com.kentoapps.ministagram.data.model
 
-class User {
-    var userId: String? = null
-    var userName: String? = null
-    var userImage: String? = null
-
-    constructor()
-
-    constructor(userId: String,
-                userName: String,
-                userImage: String) {
-        this.userId = userId
-        this.userName = userName
-        this.userImage = userImage
-    }
-}
+data class User constructor(
+        var userId: String? = null,
+        var userName: String? = null,
+        var userImage: String? = null)
