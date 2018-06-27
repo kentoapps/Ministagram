8c8a6c43d5972d01960499fd8eacb747d958fe6f
Kento Uchida <kento.apps@gmail.com>
2018-06-26 17:00:56 -0700

    Modify Post data class

--- app/src/main/kotlin/com/kentoapps/ministagram/data/model/Post.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/model/Post.kt
@@ -2,36 +2,13 @@ package com.kentoapps.ministagram.data.model
 
 import java.util.*
 
-class Post {
-    var id: String? = null
-    var userId: String? = null
-    var userName: String? = null
-    var userImage: String? = null
-    var image: String? = null
-    var caption: String? = null
-    var likeUsers: List<User> = emptyList()
-    var numOfComments: Int = 0
-    var date: Date = Date()
-
-    constructor() {}
-
-    constructor(id: String,
-                userId: String,
-                userName: String,
-                userImage: String,
-                image: String,
-                caption: String,
-                likeUsers: List<User>,
-                numOfComments: Int,
-                date: Date) {
-        this.id = id
-        this.userId = userId
-        this.userName = userName
-        this.userImage = userImage
-        this.image = image
-        this.caption = caption
-        this.likeUsers = likeUsers
-        this.numOfComments = numOfComments
-        this.date = date
-    }
-}
+data class Post constructor(
+        var id: String? = null,
+        var userId: String? = null,
+        var userName: String? = null,
+        var userImage: String? = null,
+        var image: String? = null,
+        var caption: String? = null,
+        var likeUsers: List<User> = emptyList(),
+        var numOfComments: Int = 0,
+        var date: Date = Date())
