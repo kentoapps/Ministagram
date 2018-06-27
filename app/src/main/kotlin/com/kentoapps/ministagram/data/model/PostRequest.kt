5f73330e0af60b83ed0c96109d51e9a559a55633
Kento Uchida <kento.apps@gmail.com>
2018-06-26 19:11:36 -0700

    Modify PostRequest to have user info

--- app/src/main/kotlin/com/kentoapps/ministagram/data/model/PostRequest.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/model/PostRequest.kt
@@ -2,4 +2,9 @@ package com.kentoapps.ministagram.data.model
 
 import android.net.Uri
 
-data class PostRequest(val uri: Uri, val caption: String)
+data class PostRequest(
+        val uri: Uri,
+        val caption: String,
+        val userId: String,
+        val userName: String,
+        val userImage: String)
