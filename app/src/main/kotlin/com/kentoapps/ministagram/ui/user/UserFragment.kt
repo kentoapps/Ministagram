aa34890a710c71de7d9b1f697ae070d525239399
Kento Uchida <kento.apps@gmail.com>
2018-06-29 18:46:30 -0700

    Implement sign out

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/user/UserFragment.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/user/UserFragment.kt
@@ -1,14 +1,17 @@
 package com.kentoapps.ministagram.ui.user
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kentoapps.ministagram.R
+import com.kentoapps.ministagram.databinding.UserFragmentBinding
 import com.kentoapps.ministagram.di.Injectable
+import com.kentoapps.ministagram.ui.AccountActivity
 import javax.inject.Inject
 
 class UserFragment : Fragment(), Injectable {
@@ -18,14 +21,22 @@ class UserFragment : Fragment(), Injectable {
     private val viewModel by lazy {
         ViewModelProviders.of(requireActivity(), viewModelFactory).get(UserViewModel::class.java)
     }
+    private lateinit var binding: UserFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.user_fragment, container, false)
+        binding = UserFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        binding.vm = viewModel
+
+        viewModel.successCommand.observe(this, Observer {
+            startActivity(Intent(context, AccountActivity::class.java))
+            requireActivity().finish()
+        })
     }
 
 }
--- app/src/main/kotlin/com/kentoapps/ministagram/ui/user/UserViewModel.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/user/UserViewModel.kt
@@ -1,9 +1,30 @@
 package com.kentoapps.ministagram.ui.user
 
-import android.arch.lifecycle.ViewModel;
-import com.kentoapps.ministagram.data.source.post.PostRepository
+import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import com.kentoapps.ministagram.data.source.user.UserRepository
+import com.kentoapps.ministagram.util.SingleLiveEvent
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
 
-class UserViewModel @Inject constructor(repository: PostRepository) : ViewModel() {
-    // TODO: Implement the ViewModel
+class UserViewModel @Inject constructor(private val repository: UserRepository) : ViewModel() {
+    val errorMessage = ObservableField<String>("")
+    val successCommand = SingleLiveEvent<Void>()
+
+    private val disposables = CompositeDisposable()
+
+    fun onSignOutClick() {
+        repository.signOut()
+                .subscribeBy(
+                        onComplete = { successCommand.call() },
+                        onError = { errorMessage.set(it.message) }
+                ).addTo(disposables)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.dispose()
+    }
 }
