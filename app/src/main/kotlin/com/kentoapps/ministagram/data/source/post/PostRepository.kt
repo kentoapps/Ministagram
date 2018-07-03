2230561bd7a45ad804d55189641e9daa3890532a
Kento Uchida <kento.apps@gmail.com>
2018-07-02 20:28:11 -0700

    Modify repository to combine

--- app/src/main/kotlin/com/kentoapps/ministagram/data/source/post/PostRepository.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/source/post/PostRepository.kt
@@ -4,6 +4,7 @@ import com.kentoapps.ministagram.data.model.Post
 import com.kentoapps.ministagram.data.model.PostRequest
 import io.reactivex.Completable
 import io.reactivex.Observable
+import javax.inject.Inject
 
 interface PostRepository {
     fun getPostList(): Observable<List<Post>>
@@ -11,4 +12,26 @@ interface PostRepository {
     fun savePost(post: PostRequest): Completable
     fun updatePost(post: Post): Completable
     fun deletePost(id: String): Completable
+}
+
+class PostRepositoryImpl @Inject constructor(private val dataSource: PostDataSource) : PostRepository {
+    override fun getPostList(): Observable<List<Post>> {
+        return dataSource.getPostList()
+    }
+
+    override fun getPost(id: String): Observable<Post> {
+        return dataSource.getPost(id)
+    }
+
+    override fun savePost(post: PostRequest): Completable {
+        return dataSource.savePost(post)
+    }
+
+    override fun updatePost(post: Post): Completable {
+        return dataSource.updatePost(post)
+    }
+
+    override fun deletePost(id: String): Completable {
+        return dataSource.deletePost(id)
+    }
 }
--- app/src/main/kotlin/com/kentoapps/ministagram/data/source/user/UserRepository.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/source/user/UserRepository.kt
@@ -3,6 +3,7 @@ package com.kentoapps.ministagram.data.source.user
 import com.kentoapps.ministagram.data.model.User
 import io.reactivex.Completable
 import io.reactivex.Observable
+import javax.inject.Inject
 
 interface UserRepository {
     fun isSignIn(): Observable<Boolean>
@@ -10,4 +11,26 @@ interface UserRepository {
     fun signIn(email: String, password: String): Completable
     fun signOut(): Completable
     fun getUser(): Observable<User>
+}
+
+class UserRepositoryImpl @Inject constructor(private val dataSource: UserDataSource) : UserRepository {
+    override fun isSignIn(): Observable<Boolean> {
+        return dataSource.isSignIn()
+    }
+
+    override fun signUp(userName: String, email: String, password: String): Completable {
+        return dataSource.signUp(userName, email, password)
+    }
+
+    override fun signIn(email: String, password: String): Completable {
+        return dataSource.signIn(email, password)
+    }
+
+    override fun signOut(): Completable {
+        return dataSource.signOut()
+    }
+
+    override fun getUser(): Observable<User> {
+        return dataSource.getUser()
+    }
 }
