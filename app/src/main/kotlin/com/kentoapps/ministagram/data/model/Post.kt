6944bf558f205c966ed4a013a2b93d634a8cf9e8
Kento Uchida <kento.apps@gmail.com>
2018-07-04 16:46:24 -0700

    Add isLike to Post

--- app/src/main/kotlin/com/kentoapps/ministagram/data/model/Post.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/model/Post.kt
@@ -3,12 +3,13 @@ package com.kentoapps.ministagram.data.model
 import java.util.*
 
 data class Post constructor(
-        var id: String? = null,
+        var id: String? = "test",
         var userId: String? = null,
         var userName: String? = null,
         var userImage: String? = null,
         var image: String? = null,
         var caption: String? = null,
         var likeUsers: List<User> = emptyList(),
+        var isLike: Boolean = false,
         var numOfComments: Int = 0,
         var date: Date = Date())
