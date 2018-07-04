24d7e6d109049458b56e988b865893a216c8f385
Kento Uchida <kento.apps@gmail.com>
2018-07-03 17:49:12 -0700

    Implement permission

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kentoapps.ministagram.ui
 
+import android.Manifest
 import android.app.Activity
 import android.arch.lifecycle.ViewModelProvider
 import android.content.ContentValues
@@ -20,9 +21,10 @@ import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.*
 import javax.inject.Inject
 
-
+@RuntimePermissions
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var androidInjector: DispatchingAndroidInjector<Fragment>
@@ -47,7 +49,7 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.gallery -> startActivityForResult(createIntentForGallery(), REQUEST_GALLERY)
-            R.id.camera_activity -> startActivityForResult(createIntentForCamera(), REQUEST_CAMERA)
+            R.id.camera_activity -> showCameraWithPermissionCheck()
             else -> return NavigationUI.onNavDestinationSelected(item,
                     Navigation.findNavController(this, R.id.navHostFragment))
                     || super.onOptionsItemSelected(item)
@@ -57,9 +59,20 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     private fun createIntentForGallery() = Intent(Intent.ACTION_GET_CONTENT).apply { type = "image/*" }
 
-    private fun createIntentForCamera(): Intent {
-        // TODO Need to ask permission!!
-        Toast.makeText(this, "Sorry I'll implement asking permission!!", Toast.LENGTH_SHORT).show()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) return
+
+        if (requestCode == REQUEST_GALLERY) imageUri = data?.data
+
+        if (imageUri == null) return
+        val bundle = PostFragmentArgs.Builder(imageUri.toString()).build().toBundle()
+        Navigation.findNavController(this, R.id.navHostFragment)
+                .navigate(R.id.postFragment, bundle)
+    }
+
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun showCamera() {
         val filename = System.currentTimeMillis().toString() + ".jpg"
 
         val values = ContentValues().apply {
@@ -68,29 +81,32 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
         }
         imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
 
-        return Intent().apply {
+        val intent =  Intent().apply {
             action = MediaStore.ACTION_IMAGE_CAPTURE
             putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
         }
+
+        startActivityForResult(intent, REQUEST_CAMERA)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != Activity.RESULT_OK) {
-            Toast.makeText(this, "Failed to get image", Toast.LENGTH_SHORT).show()
-            return
-        }
+    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun showRationaleForWriteExternalStorage(request: PermissionRequest) {
+        request.proceed()
+    }
 
-        when (requestCode) {
-            REQUEST_GALLERY -> {
-                imageUri = data?.data
-            }
-        }
+    @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun onWriteExternalStorageDenied() {
+        Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show()
+    }
 
-        if (imageUri == null) return
-        val bundle = PostFragmentArgs.Builder(imageUri.toString()).build().toBundle()
-        Navigation.findNavController(this, R.id.navHostFragment)
-                .navigate(R.id.postFragment, bundle)
+    @OnNeverAskAgain(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun onWriteExternalStorageNeverAskAgain() {
+        Toast.makeText(this, R.string.permission_never_ask, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
     }
 
     companion object {
