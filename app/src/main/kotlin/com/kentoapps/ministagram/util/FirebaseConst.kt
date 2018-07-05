196aff70b9ff5dad231c4e654e3f026078df393c
Kento Uchida <kento.apps@gmail.com>
2018-07-04 17:24:30 -0700

    Add FIELD_LIKE_USERS to FirebaseConst

--- app/src/main/kotlin/com/kentoapps/ministagram/util/FirebaseConst.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/util/FirebaseConst.kt
@@ -1,7 +1,12 @@
 package com.kentoapps.ministagram.util
 
+// COLLECTION
 const val COLLECTION_POST = "Post"
 const val COLLECTION_COMMENT = "Comment"
 const val COLLECTION_USER = "User"
 
+// FIELD
+const val FIELD_LIKE_USERS = "likeUsers"
+
+// STORAGE
 const val STORAGE_POSTS = "Posts"
