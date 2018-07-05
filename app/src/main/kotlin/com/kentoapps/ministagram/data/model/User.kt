cc2a16feaf1a9210e2812ea65e4cc82d89f6d3fa
Kento Uchida <kento.apps@gmail.com>
2018-07-04 17:26:26 -0700

    Add toMap to User

--- app/src/main/kotlin/com/kentoapps/ministagram/data/model/User.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/data/model/User.kt
@@ -3,4 +3,11 @@ package com.kentoapps.ministagram.data.model
 data class User constructor(
         var userId: String? = null,
         var userName: String? = null,
-        var userImage: String? = null)
+        var userImage: String? = null) {
+    fun toMap(): Map<String, Any> =
+            HashMap<String, Any>().apply {
+                userId?.let { put("userId", it) }
+                userName?.let { put("userName", it) }
+                userImage?.let { put("userImage", it) }
+            }
+}
