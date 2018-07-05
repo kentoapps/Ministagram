9d9abd7ba7d446a13a49f6f50003df57b996d1aa
Kento Uchida <kento.apps@gmail.com>
2018-07-05 00:26:47 -0700

    Implement di and dataBinding for Comment

--- app/src/main/kotlin/com/kentoapps/ministagram/ui/comment/CommentFragment.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/comment/CommentFragment.kt
@@ -1,31 +1,41 @@
 package com.kentoapps.ministagram.ui.comment
 
+import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kentoapps.ministagram.R
+import com.kentoapps.ministagram.databinding.CommentFragmentBinding
+import com.kentoapps.ministagram.di.Injectable
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
 
+class CommentFragment : Fragment(), Injectable {
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
-class CommentFragment : Fragment() {
+    private val viewModel by lazy {
+        ViewModelProviders.of(requireActivity(), viewModelFactory).get(CommentViewModel::class.java)
+    }
+    private lateinit var binding: CommentFragmentBinding
+    private val disposables = CompositeDisposable()
 
     private val postId by lazy {
         arguments?.let { CommentFragmentArgs.fromBundle(it).postId }
     }
 
-    private lateinit var viewModel: CommentViewModel
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.comment_fragment, container, false)
+        binding = CommentFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(CommentViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        binding.vm = viewModel
 
+        if (savedInstanceState == null) postId?.let { viewModel.getCommentList(it) }
+    }
 }
--- app/src/main/kotlin/com/kentoapps/ministagram/ui/comment/CommentViewModel.kt
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/comment/CommentViewModel.kt
@@ -1,7 +1,15 @@
 package com.kentoapps.ministagram.ui.comment
 
 import android.arch.lifecycle.ViewModel;
+import com.kentoapps.ministagram.data.source.post.PostRepository
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
 
-class CommentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CommentViewModel @Inject constructor(private val repository: PostRepository) : ViewModel() {
+
+    private val disposables = CompositeDisposable()
+
+    fun getCommentList(id: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
