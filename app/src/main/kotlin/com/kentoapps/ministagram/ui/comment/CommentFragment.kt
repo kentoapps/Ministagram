770664cd0037f389cfe3a0922e711dccec0ed9d1
Kento Uchida <kento.apps@gmail.com>
2018-07-04 23:46:37 -0700

    Add CommentFragment

---
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/comment/CommentFragment.kt
@@ -0,0 +1,31 @@
+package com.kentoapps.ministagram.ui.comment
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kentoapps.ministagram.R
+
+
+class CommentFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CommentFragment()
+    }
+
+    private lateinit var viewModel: CommentViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.comment_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CommentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/kotlin/com/kentoapps/ministagram/ui/comment/CommentViewModel.kt
@@ -0,0 +1,7 @@
+package com.kentoapps.ministagram.ui.comment
+
+import android.arch.lifecycle.ViewModel;
+
+class CommentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
